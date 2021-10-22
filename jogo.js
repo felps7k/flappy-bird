fcf6dfbb330a99e67ccc61941060c425f6147e19
felps7k <felipefarias97@icloud.com>
2021-10-21 23:03:43 -0300

    Update tela inicial

--- jogo.js
+++ jogo.js
@@ -6,13 +6,14 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+// ~Character~
 const character = {
-    spriteX: 0,
-    spriteY: 0,
-    width: 33,
-    height: 24,
+    sX: 0,
+    sY: 0,
+    w: 33,
+    h: 24,
     x: 10,
-    y: 200,
+    y: 220,
     speed: 0,
     gravity:0.25,
     fall(){
@@ -22,44 +23,46 @@ const character = {
     draw(){
         ctx.drawImage(
             sprites,
-            character.spriteX, character.spriteY, //Início da imagem
-            character.width, character.height, //Final da imagem
+            character.sX, character.sY, //Início da imagem
+            character.w, character.h, //Final da imagem
             character.x, character.y, //Local da imagem na tela
-            character.width, character.height //Tamanho da imagem na tela
+            character.w, character.h //Tamanho da imagem na tela
         );
     }
 }
 
+// ~Floor~
 const floor = {
-    spriteX: 0,
-    spriteY: 610,
-    width: 224,
-    height: 112,
+    sX: 0,
+    sY: 610,
+    w: 224,
+    h: 112,
     x: 0,
     y: canvas.height - 112, 
     draw(){
         ctx.drawImage(
             sprites,
-            floor.spriteX, floor.spriteY,
-            floor.width, floor.height,
+            floor.sX, floor.sY,
+            floor.w, floor.h,
             floor.x, floor.y,
-            floor.width, floor.height
+            floor.w, floor.h
         );
         ctx.drawImage(
             sprites,
-            floor.spriteX, floor.spriteY,
-            floor.width, floor.height,
-            (floor.x + floor.width), floor.y,
-            floor.width, floor.height
+            floor.sX, floor.sY,
+            floor.w, floor.h,
+            (floor.x + floor.w), floor.y,
+            floor.w, floor.h
         );
     }
 }
 
+// ~Background~
 const background = {
-    spriteX: 390,
-    spriteY: 0,
-    width: 275,
-    height: 204,
+    sX: 390,
+    sY: 0,
+    w: 275,
+    h: 204,
     x: 0,
     y: canvas.height - 204, 
     draw(){
@@ -68,28 +71,87 @@ const background = {
 
         ctx.drawImage(
             sprites,
-            background.spriteX, background.spriteY,
-            background.width, background.height,
+            background.sX, background.sY,
+            background.w, background.h,
             background.x, background.y,
-            background.width, background.height
+            background.w, background.h
         );
         ctx.drawImage(
             sprites,
-            background.spriteX, background.spriteY,
-            background.width, background.height,
-            (background.x + background.width), background.y,
-            background.width, background.height
+            background.sX, background.sY,
+            background.w, background.h,
+            (background.x + background.w), background.y,
+            background.w, background.h
         );
     }
 }
 
+// ~Get Ready Message~
+const startGamemessage = {
+    sX: 134,
+    sY: 0,
+    w: 174,
+    h: 152,
+    x: (canvas.width / 2) - 174 / 2,
+    y: 50, 
+    draw(){
+        ctx.drawImage(
+            sprites,
+            startGamemessage.sX, startGamemessage.sY,
+            startGamemessage.w, startGamemessage.h,
+            startGamemessage.x, startGamemessage.y,
+            startGamemessage.w, startGamemessage.h
+        );
+    }
+}
+
+// ~Screens~
+let activeScreen = {};
+function changeToScreen(newScreen){
+    activeScreen = newScreen;
+}
+
+const screen = {
+    START: {
+        draw(){
+            background.draw();
+            floor.draw();
+            character.draw();
+            startGamemessage.draw();
+        },
+        click(){
+            changeToScreen(screen.GAME);
+        },
+        fall(){
+
+        }
+    },
+    GAME: {
+        draw(){
+            background.draw();
+            floor.draw();
+            character.draw();
+        },
+        fall(){
+            character.fall();
+        }
+    },
+};
+
+
+
 function loop(){
-    background.draw();
-    floor.draw();
-    character.draw();
-    character.fall();
+    activeScreen.draw();
+    activeScreen.fall();
 
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function(){
+    if(activeScreen.click){
+        activeScreen.click();
+    }
+});
+
+changeToScreen(screen.START);
 loop();
