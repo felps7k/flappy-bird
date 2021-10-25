52a602b71d4197a6bda5af4c685e371d03640c20
felps7k <felipefarias97@icloud.com>
2021-10-25 19:19:50 -0300

    Updade movimentação character e floor

--- jogo.js
+++ jogo.js
@@ -1,5 +1,7 @@
 console.log('~Felps7k~');
 
+let frames = 0;
+
 const hitSound = new Audio();
 hitSound.src = './sounds/hit.wav';
 
@@ -21,9 +23,15 @@ function makeCharacter(){
         speed: 0,
         gravity:0.25,
         bounce: 4.6,
+        actualFrame: 0,
+        moves: [
+            { sX: 0, sY: 0 }, //Asa para cima
+            { sX: 0, sY: 26 }, //Asa no meio
+            { sX: 0, sY: 52 }, //Asa para baixo
+        ],
         fall(){
-            if(colision(character, floor)){
-                hitSound.play();
+            if(colision(character, global.floor)){
+                //hitSound.play(); //DESCOMENTAR PARA ATIVAR AUDIO
                 setTimeout(() =>{
                     changeToScreen(screen.START);
                 }, 500)
@@ -35,10 +43,23 @@ function makeCharacter(){
         jump(){
             character.speed = - character.bounce;
         },
+        attActualFrame(){
+            const frameInterval = 15;
+            const afterInterval = frames % frameInterval === 0;
+            if(afterInterval){
+                //const baseIncrement = 1;
+                //const i = baseIncrement + character.actualFrame;
+                const i = character.actualFrame + 1;
+                const baseRepeat = character.moves.length;
+                character.actualFrame = i % baseRepeat;
+            }
+        },
         draw(){
+            character.attActualFrame();
+            const { sX, sY } = character.moves[character.actualFrame];
             ctx.drawImage(
                 sprites,
-                character.sX, character.sY, //Início da imagem
+                sX, sY, //Início da imagem
                 character.w, character.h, //Final da imagem
                 character.x, character.y, //Local da imagem na tela
                 character.w, character.h //Tamanho da imagem na tela
@@ -60,30 +81,45 @@ function colision(character, floor){
 }
 
 // ~Floor~
-const floor = {
-    sX: 0,
-    sY: 610,
-    w: 224,
-    h: 112,
-    x: 0,
-    y: canvas.height - 112, 
-    draw(){
-        ctx.drawImage(
-            sprites,
-            floor.sX, floor.sY,
-            floor.w, floor.h,
-            floor.x, floor.y,
-            floor.w, floor.h
-        );
-        ctx.drawImage(
-            sprites,
-            floor.sX, floor.sY,
-            floor.w, floor.h,
-            (floor.x + floor.w), floor.y,
-            floor.w, floor.h
-        );
+function makeFloor(){
+    const floor = {
+        sX: 0,
+        sY: 610,
+        w: 224,
+        h: 112,
+        x: 0,
+        y: canvas.height - 112, 
+        att(){
+            const moveFloor = 1;
+            const repeatFloor = floor.w / 2;
+            const move = floor.x - moveFloor;
+            
+            if(!colision(global.character, floor)){
+                floor.x = move % repeatFloor;
+            }else {
+                return;
+            }
+        },
+        draw(){
+            ctx.drawImage(
+                sprites,
+                floor.sX, floor.sY,
+                floor.w, floor.h,
+                floor.x, floor.y,
+                floor.w, floor.h
+            );
+            ctx.drawImage(
+                sprites,
+                floor.sX, floor.sY,
+                floor.w, floor.h,
+                (floor.x + floor.w), floor.y,
+                floor.w, floor.h
+            );
+        },
     }
-}
+    return floor;
+};
+
 
 // ~Background~
 const background = {
@@ -115,7 +151,7 @@ const background = {
 }
 
 // ~Get Ready Message~
-const startGamemessage = {
+const startGameMessage = {
     sX: 134,
     sY: 0,
     w: 174,
@@ -125,10 +161,10 @@ const startGamemessage = {
     draw(){
         ctx.drawImage(
             sprites,
-            startGamemessage.sX, startGamemessage.sY,
-            startGamemessage.w, startGamemessage.h,
-            startGamemessage.x, startGamemessage.y,
-            startGamemessage.w, startGamemessage.h
+            startGameMessage.sX, startGameMessage.sY,
+            startGameMessage.w, startGameMessage.h,
+            startGameMessage.x, startGameMessage.y,
+            startGameMessage.w, startGameMessage.h
         );
     }
 }
@@ -147,41 +183,42 @@ const screen = {
     START: {
         make(){
             global.character = makeCharacter();
+            global.floor = makeFloor();
         },
         draw(){
             background.draw();
-            floor.draw();
+            global.floor.draw();
             global.character.draw();
-            startGamemessage.draw();
+            startGameMessage.draw();
         },
         click(){
             changeToScreen(screen.GAME);
         },
-        fall(){
-
+        att(){
+            global.floor.att();
         }
     },
     GAME: {
         draw(){
             background.draw();
-            floor.draw();
+            global.floor.draw();
             global.character.draw();
         },
         click(){
             global.character.jump();
         },
-        fall(){
+        att(){
             global.character.fall();
+            global.floor.att();
         }
     },
 };
 
-
-
 function loop(){
     activeScreen.draw();
-    activeScreen.fall();
+    activeScreen.att();
 
+    frames += 1;
     requestAnimationFrame(loop);
 }
 
